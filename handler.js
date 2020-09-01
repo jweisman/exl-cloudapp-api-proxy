37a56ad023ba0d8f091ce70d406855c9c0eb4d58
Josh Weisman <josh.m.weisman@gmail.com>
2020-09-01 12:34:54 +0300

    First cut

---
+++ handler.js
@@ -0,0 +1,69 @@
+require('dotenv').config();
+const utils = require('./utils');
+const { auth } = require('./authorizer/index');
+const fetch = require('node-fetch');
+const AWS = require('aws-sdk');
+const sm = new AWS.SecretsManager({region: 'us-east-1'});
+const secretName = process.env.API_KEYS_SECRET_NAME || 'ExlApiProxy/ApiKeys';
+const forInstCode = (process.env.FOR_INSTCODE_HEADER_NAME || 'X-For-InstCode').toLowerCase();
+
+const regionMap = {
+  us: 'na',
+}
+
+const handler = async (event, context) => {
+  let result;
+  if (event.requestContext && event.requestContext.http.method == 'OPTIONS') {
+    result = { statusCode: 204 };
+    return utils.cors(result, event);
+  }
+  /* Validate token */
+  const token = auth(event.headers.authorization);
+  if (!token) {
+    result = utils.responses.unauthorized();
+    return utils.cors(result, event);
+  }
+
+  const region = (process.env.AWS_REGION && process.env.AWS_REGION.slice(0,2)) || 'us';
+  const host = `api-${regionMap[region] || region}.hosted.exlibrisgroup.com`;
+  let customRequestHeader;
+  try {
+    if ( event.headers ) {
+      customRequestHeader = event.headers;
+    }
+    if (event.headers[forInstCode]) {
+      const keys = await sm.getSecretValue({SecretId: secretName}).promise();
+      if (keys) {
+        const key = JSON.parse(keys.SecretString)[event.headers[forInstCode]];
+        customRequestHeader['authorization'] = `apikey ${key}`;
+      }
+    }
+    customRequestHeader['host'] = host;
+    const params = {
+      method: event.requestContext.http.method,
+      headers: customRequestHeader,
+      body: event.body
+    };
+    const url = `https://${host}${event.rawPath}?${event.rawQueryString}`;
+    const response = await fetch(url, params);
+    const textResponse = await response.text();
+    let responseHeaders = {};
+    for (const [key, value] of response.headers.entries()) {
+      if (key != 'content-encoding') {
+        responseHeaders[key] = value;
+      }
+    }
+    result = {
+      statusCode: response.status, 
+      body: textResponse,
+      headers: responseHeaders
+    }
+  } catch (e) {
+    console.error('error', e);
+    result = utils.responses.error(e.message);
+  }
+  return utils.cors(result, event);
+};
+
+
+module.exports = { handler };
